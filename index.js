327c2c9c737a203092b658af652a587c74d60aca
sathish <sathishk@apptomate.co>
2021-09-02 19:13:40 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
-
+// /https://personal-chat-be.herokuapp.com/
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
@@ -17,55 +17,31 @@ app.get("/", (req, res) => {
   res.send("Running");
 });
 let numUsers = 0;
+let alluser = [];
 io.on("connection", (socket) => {
-  // when the client emits 'add user', this listens and executes
-  socket.on("add user", (username) => {
-    let addedUser = false;
-    if (addedUser) return;
+  socket.emit("myid", socket.id);
+  socket.on("new-join", (data) => {
+    alluser.push(data);
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit("login", {
-      numUsers: numUsers,
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit("user joined", {
-      username: socket.username,
-      numUsers: numUsers,
-    });
+    //socket.emit("All-user", alluser);
+    // io.sockets.emit("message", msg);
+    io.sockets.emit("All-user", alluser);
   });
-
-  socket.on("typing", () => {
-    console.log(socket.username, "typing");
-    socket.broadcast.emit("typing", {
-      username: socket.username,
-      value: "typing...",
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on("stop typing", () => {
-    socket.broadcast.emit("stop typing", {
-      username: socket.username,
+  socket.on("New-mesage", (data) => {
+    console.log(data);
+    io.to(data.to).emit("send-message", {
+      from: data.id,
+      message: data.message,
+      name: data.name,
+      sender: false,
     });
   });
   socket.on("disconnect", () => {
     // echo globally that this client has left
-    socket.broadcast.emit("user left", {
-      username: socket.username,
-      numUsers: numUsers,
-    });
+    alluser = alluser.filter((s) => s.id !== socket.id);
+    socket.broadcast.emit("user left", alluser);
   });
-  socket.on("new message", (data) => {
-    console.log(socket.username, data, "hi");
-    socket.broadcast.emit("new message", {
-      username: socket.username,
-      value: data,
-      self: false,
-    });
-  });
+  // when the client emits 'stop typing', we broadcast it to others
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
